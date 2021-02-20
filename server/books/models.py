09342151aeeacd120ca240cc28135f2b9e236f22
musergi <sergi.mus@gmail.com>
2021-02-20 21:18:04 +0100

    Added authors API

--- server/books/models.py
+++ server/books/models.py
@@ -5,3 +5,15 @@ class Author(models.Model):
     names = models.TextField(blank=True)
     surnames = models.TextField(blank=True)
     pseudonym = models.TextField(blank=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'names': self.names.split(' '),
+            'surnames': self.surnames.split(' '),
+            'pseudonym': self.pseudonym}
+
+    def __str__(self):
+        if self.pseudonym:
+            return self.pseudonym
+        return f'{self.names} {self.surnames}'
--- server/books/views.py
+++ server/books/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
+import json
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Author
 
 
-def author(author_id):
-    return  HttpResponse('{}', content_type='application/json')
+def author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    data = json.dumps(author.to_dict(), indent=2)
+    return  HttpResponse(data, content_type='application/json')
 
 
-def authors():
-    return  HttpResponse('{}', content_type='application/json')
+def authors(request):
+    if request.method == 'GET':
+        authors = list(Author.objects.all())
+        data = json.dumps([author.to_dict() for author in authors], indent=2)
+        return  HttpResponse(data, content_type='application/json')
+    if request.method == 'POST':
+        return HttpResponse()
+    return HttpResponseBadRequest('Method not supported.')
